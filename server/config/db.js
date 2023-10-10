96413d92e7abf1595dbd96c5e64091afd5d3dfd9
Salman Sadiq <118099469+salmansadiq1015@users.noreply.github.com>
2023-10-09 22:01:28 -0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(mongodb+srv://salman121:786salman121@cluster0.ifme6tl.mongodb.net/marketing);
     console.log(
       `Connected to MongoDB Server ${conn.connection.host}`.bgCyan.white
     );
